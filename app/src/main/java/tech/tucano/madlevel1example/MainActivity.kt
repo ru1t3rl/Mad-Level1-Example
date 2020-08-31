14cf46022ea8799fde244dc9abf2b2b803f7e407
ru1t3rl <luca.ruiters@hva.nl>
2020-08-31 11:18:16 +0200

    Added Feedback and User Input

--- app/src/main/java/tech/tucano/madlevel1example/MainActivity.kt
+++ app/src/main/java/tech/tucano/madlevel1example/MainActivity.kt
@@ -2,10 +2,29 @@ package tech.tucano.madlevel1example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import tech.tucano.madlevel1example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnConfirm.setOnClickListener{
+            checkAnswer()
+        }
+    }
+
+    private fun checkAnswer(){
+        val ans = binding.etAnswer.text.toString()
+
+        if (ans == getString(R.string.giraffe)){
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
     }
 }
